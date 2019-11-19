24d52441255e4a03f3300380f62a2ca01d5ccf1c
dmitriy.stiopa <dmitriy.stiopa@gmail.com>
2019-11-19 17:27:03 +0200

    Added balance manipulation, moved logic to service

--- api/controllers/transactionController.js
+++ api/controllers/transactionController.js
@@ -1,7 +1,7 @@
 'use strict';
 
-import moment from 'moment';
 import mongoose from 'mongoose'
+import { walletService } from '../services/wallet.service'
 
 const Transaction = mongoose.model('Transaction'),
     Wallet = mongoose.model('Wallet'),
@@ -9,11 +9,7 @@ const Transaction = mongoose.model('Transaction'),
 
 exports.list = async (req, res, next) => {
 
-    const currWallet = await ActiveWallet.findOne({}, (err, model) => {
-        if (err) {
-            next(err);
-        }
-    })
+    const currWallet = await walletService.getActiveWallet();
 
     Transaction.find({ walletId: currWallet.walletId }, (err, tran) => {
         if (err) {
@@ -24,43 +20,19 @@ exports.list = async (req, res, next) => {
 }
 
 exports.add_transaction = async (req, res, next) => {
-    let transaction = new Transaction(req.body);
-    let currWallet = await ActiveWallet.findOne({}, (err, model) => {
-        if (err) {
-            next(err);
-        }
-    })
+    const transaction = new Transaction(req.body);
+
+    const currWallet = await walletService.getActiveWallet();
 
     transaction.walletId = currWallet.walletId;
 
-    let wallet = await Wallet.findById(currWallet.walletId, (err, wallet) => {
-        if (err) {
-            next(err)
-        }
-    })
-
-    if (transaction.type === 'outgoing') {
-        wallet.balance -= transaction.value;
-    } else {
-        wallet.balance += transaction.value;
-    }
-
-    if (moment(transaction.date).isAfter(moment(wallet.lastExpenses.date))) {
-        wallet.lastExpenses = {
-            date: transaction.date,
-            value: transaction.value,
-            currency: transaction.currency,
-            category: transaction.category,
-            type: transaction.type
-        }
-    }
-
-    let session = await mongoose.startSession();
+    const session = await mongoose.startSession();
 
     try {
         await session.startTransaction();
 
-        await wallet.save();
+        await walletService.changeLastActionIfEarliest(currWallet.walletId, transaction);
+        await walletService.updateWalletBalance(currWallet.walletId, transaction);
         await transaction.save();
 
         await session.commitTransaction();
@@ -73,13 +45,47 @@ exports.add_transaction = async (req, res, next) => {
     }
 }
 
-exports.edit_transaction = async (req, res) => {
-    let transaction = await Transaction.findById(req.body.id);
-    await transaction.update(req.body);
-    res.send(req.body);
+exports.edit_transaction = async (req, res, next) => {
+    const transaction = await Transaction.findById(req.body._id);
+
+    const currWallet = await walletService.getActiveWallet();
+    const session = await mongoose.startSession();
+    try {
+        await session.startTransaction();
+
+        await walletService.changeLastActionIfEarliest(currWallet.walletId, req.body);
+        await walletService.updateWalletBalanceEdit(currWallet.walletId, transaction, req.body);
+        await transaction.update(req.body);
+
+        await session.commitTransaction();
+
+        res.send(req.body);
+    } catch (err) {
+        session.abortTransaction();
+
+        next(err);
+    }
 }
 
-exports.delete = async (req, res) => {
-    await Transaction.findByIdAndDelete(req.query.id);
-    res.send({ success: true, data: req.query.id })
+exports.delete = async (req, res, next) => {
+    const transaction = await Transaction.findById(req.query.id);
+
+    const currWallet = await walletService.getActiveWallet();
+    const session = await mongoose.startSession();
+
+    try {
+        await session.startTransaction();
+
+        await walletService.changeLastActionIfEarliest(currWallet.walletId, req.body);
+        await walletService.updateWalletBalanceDelete(currWallet.walletId, transaction);
+        await Transaction.findByIdAndDelete(req.query.id);
+
+        await session.commitTransaction();
+
+        res.send({ success: true, data: req.query.id })
+    } catch (err) {
+        session.abortTransaction();
+
+        next(err);
+    }
 }
--- api/services/wallet.service.js
+++ api/services/wallet.service.js
@@ -1,15 +1,68 @@
 import mongoose from 'mongoose';
+import moment from 'moment';
 
 class WalletService {
 
     constructor() {
-        console.error('asddasdas');
         this._walletRepository = mongoose.model('Wallet');
+        this._activeWalletRepository = mongoose.model('ActiveWallet');
     }
 
     async findAll() {
-        //await this._walletRepository.insertMany([{}]);
         return await this._walletRepository.find({});
+    }
+
+    async changeLastActionIfEarliest(walletId, newTransaction) {
+        const wallet = await this._walletRepository.findById(walletId);
+        if (moment(newTransaction.date).isAfter(moment(wallet.lastExpenses.date)) || moment(newTransaction.date).isSame(moment(wallet.lastExpenses.date))) {
+            wallet.lastExpenses = {
+                date: newTransaction.date,
+                value: newTransaction.value,
+                currency: newTransaction.currency,
+                category: newTransaction.category,
+                type: newTransaction.type
+            };
+            return wallet.save();
+        }
+    }
+
+    async getActiveWallet() {
+        return this._activeWalletRepository.findOne({});
+    }
+
+    async updateWalletBalance(walletId, transaction) {
+        const wallet = await this._walletRepository.findById(walletId);
+        if (transaction.type === 'outgoing') {
+            wallet.balance -= +transaction.value;
+        } else {
+            wallet.balance += +transaction.value;
+        }
+        return wallet.save();
+    }
+
+    async updateWalletBalanceEdit(walletId, oldTran, newTran) {
+        const wallet = await this._walletRepository.findById(walletId);
+        if (oldTran.type === 'outgoing') {
+            wallet.balance += +oldTran.value;
+        } else {
+            wallet.balance -= +oldTran.value;
+        }
+        if (newTran.type === 'outgoing') {
+            wallet.balance -= +newTran.value;
+        } else {
+            wallet.balance += +newTran.value;
+        }
+        return wallet.save();
+    }
+
+    async updateWalletBalanceDelete(walletId, transaction) {
+        const wallet = await this._walletRepository.findById(walletId);
+        if (transaction.type === 'outgoing') {
+            wallet.balance += +transaction.value;
+        } else {
+            wallet.balance -= +transaction.value;
+        }
+        return wallet.save();
     }
 }
 
